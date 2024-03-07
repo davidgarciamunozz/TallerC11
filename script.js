3da7fd425959805ed1903fa2734773fce14005c0
DavidG <garxia0710@gmail.com>
2024-03-07 07:23:33 -0500

    script

---
+++ script.js
@@ -0,0 +1,20 @@
+const Name = prompt("Type your name");
+
+
+const LastName = prompt("Type your last name");
+
+
+let BirthYear = prompt("Type year of birth"); 
+BirthYear = parseInt(BirthYear);
+
+
+const Age = (2024-BirthYear);
+
+
+const BirhtDay = prompt("Type your birth day");
+
+
+const BirthMonth = prompt("Type your month of birth");
+
+
+console.log("My name is ", Name, LastName, " my age is ", Age, "years old and i was born on ", BirthMonth, BirhtDay, BirthYear);
